1a209eb3dd016526f32b5b8b175164c0d4bd47bb
junhyun <erik1201@naver.com>
2023-11-18 15:23:36 +0900

    객체 등록 json으로 하도록 전부 수정

--- JHGPT/src/main/java/com/jhgpt/controller/MemberRestController.java
+++ JHGPT/src/main/java/com/jhgpt/controller/MemberRestController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +24,6 @@ import com.jhgpt.model.service.MemberService;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/api")
